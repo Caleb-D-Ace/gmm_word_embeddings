04421acafaf63a718d508d676c7a6f47a7e92a7a
Caleb-D-Ace <caleb.drake@byu.edu>
2026-08-11 15:57:04 -0600

    Implemented dataset for accessing indexed corpus and getting ctx tokens

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,30 @@
+import numpy as np
+import torch
 """
-SkipGramDataset maps the information created by preprocess.py in corpus_ids.bin to a numpy .memmap format.
+SkipGramDataset uses .memmap to read an indexed corpus .bin to get center and ctx words from our corpus.
 """
 def SkipGramDataset():
-    return
+    def __init__(self, bin_file: str, window_size: int = 5, dtype = np.uint16):
+        # memmap allows us to read the corpus without loading it all into RAM
+        self.data = np.memmap(bin_file, dtype = dtype, mode = 'r')
+        self.window_size = window_size
+        self.data_len = len(self.data)
+
+    def __len__(self):
+        # Adjust the length by the size of the window, since we don't want to get an index out-of-bounds error.
+        return self.data_len - (2 * self.window_size)
+
+    def __getitem__(self, idx):
+        # Get center word
+        center_idx = idx + self.window_size
+        center_word = self.data[center_idx]
+        
+        # Get context words
+        left_bound = idx
+        right_bound = center_idx + self.window_size
+
+        left_ctx = self.data[left_bound : center_idx]
+        right_ctx = self.data[center_idx + 1 : right_bound]
+
+        ctx_words = np.concatenate([left_ctx, right_ctx])
+        return torch.tensor(center_word, dtype=torch.long), torch.tensor(ctx_words, dtype=torch.long)
