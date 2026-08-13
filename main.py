91da597de285bd5087fef0b4452b07be5e04267c
Caleb-D-Ace <caleb.drake@byu.edu>
2026-08-13 13:57:05 -0600

    Basic file setup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 """
 This is the main entry point for the training model.
 Instructions:
+When processing the corpus to build a vocabulary, words with a frequency count below a given threshold are cut from our final vocabulary. 
+This removes typos as well as very rare words that may not have enough context information to create good mappings. By default, this threshold is set to 40.
+Press "Enter" to keep the default. Otherwise, type a new threshold and press "Enter".
 """
--- src/gmm_training.py
+++ src/gmm_training.py
@@ -1,5 +1,5 @@
 """
-GmmTrainer runs through the .memmap file created by gmm_model.py, passes its data into PyTorch, and trains a probablistic 
+GmmTrainer runs through the .memmap file created by pt_model.py, passes its data into PyTorch, and trains a probablistic 
 word embedding model using the energy math from gmm_model.py. 
 It outputs a .npz file containing the fully-trained model.
 """
