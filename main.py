c9800bf9799e004fb728f0be62bed3f3548328d6
Caleb D <cadragon11@gmail.com>
2026-09-01 09:59:04 -0600

    Main method began

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ When processing the corpus to build a vocabulary, words with a frequency count b
 This removes typos as well as very rare words that may not have enough context information to create good mappings. By default, this threshold is set to 40.
 Press "Enter" to keep the default. Otherwise, type a new threshold and press "Enter".
 """
+import argparse
+import dataset
+from torch.utils.data import DataLoader
+
+def main():
+    parser = argparse.ArgumentParser(description="Train GMM Word Embeddings")
+    
+    # Default batch size is 256. This can be overridden by the user via command line argument.
+    parser.add_argument(
+        "--batch_size", 
+        type=int, 
+        default=256, 
+        help="Batch size for training (default: 256)"
+    )
+    
+    args = parser.parse_args()
+
+    # Pass the dynamic batch_size into your DataLoader
+    dataloader = DataLoader(
+        dataset, 
+        batch_size=args.batch_size, 
+        shuffle=True
+    )
