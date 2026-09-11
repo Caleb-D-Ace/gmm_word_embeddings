69bd9309f5b19e09d2a6f4992780be4c8e97128d
Caleb D <cadragon11@gmail.com>
2026-09-11 16:06:25 -0600

    Added code to read json vocab, instantiated necessary training components

--- src/gmm_training.py
+++ src/gmm_training.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import json
+from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
 
@@ -34,8 +36,23 @@ class GmmTrainer:
 
     def train(self):
         # 2. Load vocabulary and metadata
-
+        # Verify that the processed directory contains needed files
+        if not Path(self.config.processed_dir).exists():
+            raise FileNotFoundError(f"Processed directory '{self.config.processed_dir}' does not exist. Please run the preprocess command first.")
+        if not Path(self.config.processed_dir, "sorted_vocab.json").exists():
+            raise FileNotFoundError(f"Vocabulary file 'sorted_vocab.json' not found in '{self.config.processed_dir}'. Please run the preprocess command first.")
+        if not Path(self.config.processed_dir, "corpus_index.bin").exists():
+            raise FileNotFoundError(f"Corpus index file 'corpus_index.bin' not found in '{self.config.processed_dir}'. Please run the preprocess command first.")
+        
+        with open(Path(self.config.processed_dir) / "sorted_vocab.json", "r", encoding="utf-8") as f:
+            vocab = json.load(f)
+            vocab_size = len(vocab)
+        word_count_list = [count for rank, count in sorted(vocab.values(), key=lambda x: x[0])]
+        
         # 3. Instantiate pipelines
+        embedding_model = GMMWordEmbedding(vocab_size, self.config.embedding_dim, self.config.K).to(self.device)
+        dataset = SkipGramDataset(bin_file=Path(self.config.processed_dir) / "corpus_index.bin", window_size=self.config.window_size)
+        sampler = NegativeSampler(vocab_size, word_counts=word_count_list)
 
         # 4. Training loop
 
