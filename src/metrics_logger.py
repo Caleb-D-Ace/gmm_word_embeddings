75d818328c64827285659420e9e7619a40dbf1b3
Caleb D <cadragon11@gmail.com>
2026-09-23 17:19:37 -0600

    Stubbed initial metrics logger class

---
+++ src/metrics_logger.py
@@ -0,0 +1,17 @@
+import csv
+import logging
+from pathlib import Path
+
+class MetricsLogger:
+
+    def __init__(self):
+        pass
+
+    def openLog(self):
+        pass
+
+    def instantiateLog(self):
+        pass
+
+    def writeRow(self):
+        pass
